7c8bf95eb4f022ac9555bc445efa719e3c37e803
jupitters <sampaiobale@gmail.com>
2025-11-13 14:48:01 -0300

    admin model

--- src/main/java/com/jupitters/universalpetcare/model/Admin.java
+++ src/main/java/com/jupitters/universalpetcare/model/Admin.java
@@ -1,4 +1,13 @@
 package com.jupitters.universalpetcare.model;
 
 public class Admin {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String userType;
+    private boolean isEnabled;
 }
