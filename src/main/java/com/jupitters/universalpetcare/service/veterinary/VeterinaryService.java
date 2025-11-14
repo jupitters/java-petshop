893ca68f289e4af6e2de3221b06f29b6eed3b96a
jupitters <sampaiobale@gmail.com>
2025-11-14 17:50:21 -0300

    create veterianrian

--- src/main/java/com/jupitters/universalpetcare/service/veterinary/VeterinaryService.java
+++ src/main/java/com/jupitters/universalpetcare/service/veterinary/VeterinaryService.java
@@ -3,11 +3,17 @@ package com.jupitters.universalpetcare.service.veterinary;
 import com.jupitters.universalpetcare.model.User;
 import com.jupitters.universalpetcare.model.Veterinary;
 import com.jupitters.universalpetcare.request.CreateUserRequest;
+import com.jupitters.universalpetcare.service.user.UserAttributesMapper;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class VeterinaryService implements IVeterinaryService{
+    private final UserAttributesMapper userAttributesMapper;
+
     @Override
     public User createVeterinary(CreateUserRequest request) {
         Veterinary veterinary = new Veterinary();
-        userAttributesMapper
+        userAttributesMapper.setCommonAttributes(request, veterinary);
+        veterinary.setSpecialization(request.getSpecialization());
     }
 }
