7928c54368502b8ab1ea84cd793fb29ea65ad158
jupitters <sampaiobale@gmail.com>
2025-11-14 17:15:08 -0300

    admin repository

--- src/main/java/com/jupitters/universalpetcare/repository/AdminRepository.java
+++ src/main/java/com/jupitters/universalpetcare/repository/AdminRepository.java
@@ -1,4 +1,7 @@
 package com.jupitters.universalpetcare.repository;
 
-public interface AdminRepository {
+import com.jupitters.universalpetcare.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
 }
