310d7921abe377b323c5dd8e2ccfffda0d6a25ba
jupitters <sampaiobale@gmail.com>
2025-11-14 17:50:53 -0300

    create veterianrian

--- src/main/java/com/jupitters/universalpetcare/service/user/UserAttributesMapper.java
+++ src/main/java/com/jupitters/universalpetcare/service/user/UserAttributesMapper.java
@@ -4,6 +4,15 @@ import com.jupitters.universalpetcare.model.User;
 import com.jupitters.universalpetcare.request.CreateUserRequest;
 
 public class UserAttributesMapper {
-
+    public void setCommonAttributes(CreateUserRequest source, User target) {
+        target.setFirstName(source.getFirstName());
+        target.setLastName(source.getLastName());
+        target.setGender(source.getGender());
+        target.setPhoneNumber(source.getPhoneNumber());
+        target.setEmail(source.getEmail());
+        target.setPassword(source.getPassword());
+        target.setUserType(source.getUserType());
+        target.setEnabled(source.isEnabled());
+        target.setSpecialization(source.getSpecialization());
     }
 }
