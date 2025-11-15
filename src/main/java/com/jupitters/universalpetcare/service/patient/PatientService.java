51f90e023787bed3b6c1cddecbb572c8d0e2c5f8
jupitters <sampaiobale@gmail.com>
2025-11-15 18:26:33 -0300

    create patient

--- src/main/java/com/jupitters/universalpetcare/service/patient/PatientService.java
+++ src/main/java/com/jupitters/universalpetcare/service/patient/PatientService.java
@@ -1,4 +1,19 @@
 package com.jupitters.universalpetcare.service.patient;
 
-public class PatientService {
+import com.jupitters.universalpetcare.model.Patient;
+import com.jupitters.universalpetcare.model.User;
+import com.jupitters.universalpetcare.request.CreateUserRequest;
+import com.jupitters.universalpetcare.service.user.UserAttributesMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService implements IPatientService {
+    private UserAttributesMapper userAttributesMapper;
+
+    @Override
+    public User createPatient(CreateUserRequest request) {
+        Patient patient = new Patient();
+        userAttributesMapper.setCommonAttributes(request, patient);
+        return patient;
+    }
 }
