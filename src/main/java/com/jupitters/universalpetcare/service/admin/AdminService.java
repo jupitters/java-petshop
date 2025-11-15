9125c4c5ec050bd2eec35e5f940b6d7b1e69e7fd
jupitters <sampaiobale@gmail.com>
2025-11-15 18:28:23 -0300

    create admin

--- src/main/java/com/jupitters/universalpetcare/service/admin/AdminService.java
+++ src/main/java/com/jupitters/universalpetcare/service/admin/AdminService.java
@@ -1,4 +1,20 @@
 package com.jupitters.universalpetcare.service.admin;
 
-public class AdminService {
+import com.jupitters.universalpetcare.model.Admin;
+import com.jupitters.universalpetcare.model.User;
+import com.jupitters.universalpetcare.request.CreateUserRequest;
+import com.jupitters.universalpetcare.service.user.UserAttributesMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService implements IAdminService{
+    private UserAttributesMapper userAttributesMapper;
+
+    @Override
+    public User createAdmin(CreateUserRequest request) {
+        Admin admin = new Admin();
+        userAttributesMapper.setCommonAttributes(request, admin);
+
+        return admin;
+    }
 }
--- src/main/java/com/jupitters/universalpetcare/service/admin/IAdminService.java
+++ src/main/java/com/jupitters/universalpetcare/service/admin/IAdminService.java
@@ -1,4 +1,8 @@
 package com.jupitters.universalpetcare.service.admin;
 
+import com.jupitters.universalpetcare.model.User;
+import com.jupitters.universalpetcare.request.CreateUserRequest;
+
 public interface IAdminService {
+    public User createAdmin(CreateUserRequest request);
 }
