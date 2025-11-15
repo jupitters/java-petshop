53c5022f0645fb17a3ea3978924f141529123518
jupitters <sampaiobale@gmail.com>
2025-11-15 20:09:36 -0300

    config

--- src/main/java/com/jupitters/universalpetcare/dto/EntityConverter.java
+++ src/main/java/com/jupitters/universalpetcare/dto/EntityConverter.java
@@ -1,7 +1,15 @@
 package com.jupitters.universalpetcare.dto;
 
+import org.modelmapper.ModelMapper;
+
 public class EntityConverter<T, D>{
     private ModelMapper modelMapper;
 
-    public D map
+    public D mapEntityToDto(T entity, Class<D> dtoClass){
+        return modelMapper.map(entity, dtoClass);
+    }
+
+    public T mapDtoToEntity(D dto, Class<T> entityClass){
+        return modelMapper.map(dto, entityClass);
+    }
 }
