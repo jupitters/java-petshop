1eaeea09ea70350f925dba4d477003b67f9fc542
jupitters <sampaiobale@gmail.com>
2025-11-15 20:27:24 -0300

    user controller

--- src/main/java/com/jupitters/universalpetcare/controller/UserController.java
+++ src/main/java/com/jupitters/universalpetcare/controller/UserController.java
@@ -2,17 +2,21 @@ package com.jupitters.universalpetcare.controller;
 
 import com.jupitters.universalpetcare.dto.EntityConverter;
 import com.jupitters.universalpetcare.dto.UserDto;
+import com.jupitters.universalpetcare.exceptions.ResourceAlreadyExistsException;
 import com.jupitters.universalpetcare.model.User;
 import com.jupitters.universalpetcare.request.CreateUserRequest;
 import com.jupitters.universalpetcare.response.ApiResponse;
 import com.jupitters.universalpetcare.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,8 +27,13 @@ public class UserController {
 
     @PostMapping("/add")
     public ResponseEntity<ApiResponse> add(@RequestBody CreateUserRequest request){
-        User user = userService.createUser(request);
-        UserDto userDto = entityConverter.mapEntityToDto(user, UserDto.class);
-        return ResponseEntity.ok(new ApiResponse("Success!", userDto));
+        try {
+            User user = userService.createUser(request);
+            UserDto userDto = entityConverter.mapEntityToDto(user, UserDto.class);
+            return ResponseEntity.ok(new ApiResponse("Success!", userDto));
+        } catch (ResourceAlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
     }
 }
