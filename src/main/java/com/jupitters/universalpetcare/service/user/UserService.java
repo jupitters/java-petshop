efcaeda464d8c001c1c11b831790662e3335d3d3
jupitters <sampaiobale@gmail.com>
2025-11-15 18:29:17 -0300

    create user

--- src/main/java/com/jupitters/universalpetcare/service/user/UserService.java
+++ src/main/java/com/jupitters/universalpetcare/service/user/UserService.java
@@ -21,6 +21,23 @@ public class UserService implements IUserService{
 
     @Override
     public User createUser(CreateUserRequest request){
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new ResourceAlreadyExistsException("Oops, " + request.getEmail() + " is already registered!");
+        }
 
+        switch (request.getUserType()){
+            case "VET" -> {
+                return veterinaryService.createVeterinary(request);
+            }
+            case "PATIENT" -> {
+                return patientService.createPatient(request);
+            }
+            case "ADMIN" -> {
+                return adminService.createAdmin(request);
+            }
+            default -> {
+                return null;
+            }
+        }
     }
 }
