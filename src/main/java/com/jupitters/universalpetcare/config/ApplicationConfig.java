4791fd4d1989c9ca4e5385ff7e65677936c58a2e
jupitters <sampaiobale@gmail.com>
2025-11-18 17:32:35 -0300

    confgi

--- src/main/java/com/jupitters/universalpetcare/config/ApplicationConfig.java
+++ src/main/java/com/jupitters/universalpetcare/config/ApplicationConfig.java
@@ -6,8 +6,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
+
 }
