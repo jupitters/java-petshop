0b4e711b6407b22bf16f937a443fe0e47d42fe66
jupitters <sampaiobale@gmail.com>
2025-11-17 23:35:40 -0300

    user mapper

--- src/main/java/com/jupitters/universalpetcare/service/user/UserAttributesMapper.java
+++ src/main/java/com/jupitters/universalpetcare/service/user/UserAttributesMapper.java
@@ -6,5 +6,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserAttributesMapper {
-
+    public void setCommonAttributes(CreateUserRequest source, User target) {
+        target.setFirstName(source.getFirstName());
+        target.setLastName(source.getLastName());
+        target.setGender(source.getGender());
+        target.setPhoneNumber(source.getPhoneNumber());
+        target.setEmail(source.getEmail());
+        target.setPassword(source.getPassword());
+        target.setUserType(source.getUserType());
+        target.setEnabled(source.isEnabled());
+        target.setSpecialization(source.getSpecialization());
+    }
 }
