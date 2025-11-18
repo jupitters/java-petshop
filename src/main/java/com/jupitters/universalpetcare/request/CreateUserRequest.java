755025526e164f69fe5f369afd7ddfb3cbe7f3b1
jupitters <sampaiobale@gmail.com>
2025-11-17 23:36:56 -0300

    user request

--- src/main/java/com/jupitters/universalpetcare/request/CreateUserRequest.java
+++ src/main/java/com/jupitters/universalpetcare/request/CreateUserRequest.java
@@ -4,13 +4,5 @@ import lombok.Data;
 
 @Data
 public class CreateUserRequest {
-    private String firstName;
-    private String lastName;
-    private String gender;
-    private String phoneNumber;
-    private String email;
-    private String password;
-    private String userType;
-    private boolean isEnabled;
-    private String specialization;
+
 }
