d4256b16dfb0bb1bdd4e2fd78ff85b2a834c31df
jupitters <sampaiobale@gmail.com>
2025-11-18 22:43:12 -0300

    user update service

--- src/main/java/com/jupitters/universalpetcare/service/user/IUserService.java
+++ src/main/java/com/jupitters/universalpetcare/service/user/IUserService.java
@@ -3,7 +3,10 @@ package com.jupitters.universalpetcare.service.user;
 import com.jupitters.universalpetcare.exceptions.ResourceAlreadyExistsException;
 import com.jupitters.universalpetcare.model.User;
 import com.jupitters.universalpetcare.request.CreateUserRequest;
+import com.jupitters.universalpetcare.request.UpdateUserRequest;
 
 public interface IUserService {
     User createUser(CreateUserRequest request) throws ResourceAlreadyExistsException;
+
+    User updateUser(Long userId, UpdateUserRequest request);
 }
--- src/main/java/com/jupitters/universalpetcare/service/user/UserService.java
+++ src/main/java/com/jupitters/universalpetcare/service/user/UserService.java
@@ -42,6 +42,7 @@ public class UserService implements IUserService{
         }
     }
 
+    @Override
     public User updateUser(Long userId, UpdateUserRequest request) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
