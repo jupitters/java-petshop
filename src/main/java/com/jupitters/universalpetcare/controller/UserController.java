d217eb24b6a5f51a549a5ee9a9d324d8673df060
jupitters <sampaiobale@gmail.com>
2025-11-18 22:49:35 -0300

    user controller

--- src/main/java/com/jupitters/universalpetcare/controller/UserController.java
+++ src/main/java/com/jupitters/universalpetcare/controller/UserController.java
@@ -5,16 +5,14 @@ import com.jupitters.universalpetcare.dto.UserDto;
 import com.jupitters.universalpetcare.exceptions.ResourceAlreadyExistsException;
 import com.jupitters.universalpetcare.model.User;
 import com.jupitters.universalpetcare.request.CreateUserRequest;
+import com.jupitters.universalpetcare.request.UpdateUserRequest;
 import com.jupitters.universalpetcare.response.ApiResponse;
 import com.jupitters.universalpetcare.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -39,5 +37,11 @@ public class UserController {
             return ResponseEntity.status(INTERNAL_SERVER_ERROR)
                     .body(new ApiResponse(e.getMessage(), null));
         }
+    }
+
+    public ResponseEntity<ApiResponse> updateUser(@PathVariable Long userId, @RequestBody UpdateUserRequest request){
+        User oldUser = userService.updateUser(userId, request);
+        UserDto updatedUser = entityConverter.mapEntityToDto(oldUser, UserDto.class);
+        return ResponseEntity.ok(new ApiResponse("Updated Successfully!", updatedUser));
     }
 }
