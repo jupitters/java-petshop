140e4ef297c4e27d32ff0498195560c28a1fcab0
jupitters <sampaiobale@gmail.com>
2025-11-19 17:52:36 -0300

    appointment model

--- src/main/java/com/jupitters/universalpetcare/model/Appointment.java
+++ src/main/java/com/jupitters/universalpetcare/model/Appointment.java
@@ -18,5 +18,12 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @Entity
 public class Appointment {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String reason;
+    private LocalDate date;
+    private LocalTime time;
+    private String appointmentNo;
+    private LocalDate createdAt;
 }
