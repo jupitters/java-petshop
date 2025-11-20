7c667bf17840f31744cf798b017ed044ed88bfbb
jupitters <sampaiobale@gmail.com>
2025-11-19 23:53:36 -0300

    appointment add

--- src/main/java/com/jupitters/universalpetcare/model/Appointment.java
+++ src/main/java/com/jupitters/universalpetcare/model/Appointment.java
@@ -40,4 +40,12 @@ public class Appointment {
         }
         sender.getAppointments().add(this);
     }
+
+    public void addVeterinarian(User recipient){
+        this.setVeterinarian(recipient);
+        if (recipient.getAppointments() == null) {
+            recipient.setAppointments(new ArrayList<Appointment>());
+        }
+        recipient.getAppointments().add(this);
+    }
 }
