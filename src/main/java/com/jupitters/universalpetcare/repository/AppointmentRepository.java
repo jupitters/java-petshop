7078b1b4f9162d745491093f4c4b0ac6cb4d07b8
jupitters <sampaiobale@gmail.com>
2025-11-20 00:13:57 -0300

    appointment repository

---
+++ src/main/java/com/jupitters/universalpetcare/repository/AppointmentRepository.java
@@ -0,0 +1,7 @@
+package com.jupitters.universalpetcare.repository;
+
+import com.jupitters.universalpetcare.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
