9142c84e05ef69913f50ef7c2efe74be15d477b7
jupitters <sampaiobale@gmail.com>
2025-11-20 00:08:29 -0300

    appointment service

--- src/main/java/com/jupitters/universalpetcare/service/appointment/AppointmentService.java
+++ src/main/java/com/jupitters/universalpetcare/service/appointment/AppointmentService.java
@@ -2,5 +2,36 @@ package com.jupitters.universalpetcare.service.appointment;
 
 import com.jupitters.universalpetcare.model.Appointment;
 
+import java.util.List;
+
 public class AppointmentService implements IAppointmentService {
+    @Override
+    public Appointment createAppointment(Appointment appointment, Long sender, Long recipient) {
+        return null;
+    }
+
+    @Override
+    public List<Appointment> getAllAppointments(Appointment appointment) {
+        return List.of();
+    }
+
+    @Override
+    public Appointment updateAppointment(Long id, UpdateAppointmentRequest request) {
+        return null;
+    }
+
+    @Override
+    public void deleteAppointment(Long id) {
+
+    }
+
+    @Override
+    public Appointment getAppointmentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Appointment getAppointmentByNo(String appointmentNo) {
+        return null;
+    }
 }
