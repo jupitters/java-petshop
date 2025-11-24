8776d3e864025f26fecaadbe2b6da48984bcf8ba
jupitters <sampaiobale@gmail.com>
2025-11-23 22:58:58 -0300

    user dto

--- src/main/java/com/jupitters/universalpetcare/dto/UserDto.java
+++ src/main/java/com/jupitters/universalpetcare/dto/UserDto.java
@@ -5,13 +5,4 @@ import lombok.Data;
 
 @Data
 public class UserDto {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String gender;
-    private String phoneNumber;
-    private String email;
-    private String userType;
-    private boolean isEnabled;
-    private String specialization;
-}
+
