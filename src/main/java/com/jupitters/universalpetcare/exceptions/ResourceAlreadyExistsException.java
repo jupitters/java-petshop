54930ca2a68f714f4b2208a657b507b080f21f95
jupitters <sampaiobale@gmail.com>
2025-11-23 22:59:30 -0300

    user dto

--- src/main/java/com/jupitters/universalpetcare/exceptions/ResourceAlreadyExistsException.java
+++ src/main/java/com/jupitters/universalpetcare/exceptions/ResourceAlreadyExistsException.java
@@ -1,7 +1,5 @@
 package com.jupitters.universalpetcare.exceptions;
 
 public class ResourceAlreadyExistsException extends RuntimeException {
-    public ResourceAlreadyExistsException(String s) {
-        super(s);
-    }
+
 }
