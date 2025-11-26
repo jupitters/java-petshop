4c9f2ababbdfdd62f3e7686af8758f84e62cf5f4
jupitters <sampaiobale@gmail.com>
2025-11-25 22:29:18 -0300

    appointment service

--- src/main/java/com/jupitters/universalpetcare/service/appointment/AppointmentService.java
+++ src/main/java/com/jupitters/universalpetcare/service/appointment/AppointmentService.java
@@ -19,7 +19,19 @@ public class AppointmentService implements IAppointmentService {
     private final AppointmentRepository appointmentRepository;
     private final UserRepository userRepository;
 
-
+    @Override
+    public Appointment createAppointment(Appointment appointment, Long senderId, Long recipientId) {
+        Optional<User> sender = userRepository.findById(senderId);
+        Optional<User> recipient = userRepository.findById(recipientId);
+        if (sender.isPresent() && recipient.isPresent()) {
+            appointment.setPatient(sender.get());
+            appointment.setVeterinarian(recipient.get());
+            appointment.setAppointmentNo();
+            appointment.setStatus(AppointmentStatus.WAITING_FOR_APPROVAL);
+            return appointmentRepository.save(appointment);
+        }
+        return null;
+    }
 
     @Override
     public List<Appointment> getAllAppointments(Appointment appointment) {
